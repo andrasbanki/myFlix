52ae3d303e97510a033b86be298bdcc37891b112
andrasbanki <abankiandras@gmail.com>
2021-06-06 13:13:01 +0200

    Added validation and changed the port

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const uuid = require('uuid');
-
+const cors = require('cors');
 const morgan = require('morgan');
 const app = express();
-
+const {check, validationResult} = require('express-validator');
 const mongoose = require('mongoose');
 const Models = require('./models.js');
 let auth = require('./auth.js')(app);
@@ -18,11 +18,22 @@ const genres = Models.Genre;
 
 mongoose.connect('mongodb://localhost:27017/myFlixDB', { useNewUrlParser: true, useUnifiedTopology: true });
 
-// logging
-
 app.use(morgan('common'));
 app.use(bodyParser.json());
 
+let allowedOrigins = ['http://localhost:8080', 'http://testsite.com'];
+
+app.use(cors({
+  origin: (origin, callback) => {
+    if(!origin) return callback(null, true);
+    if(allowedOrigins.indexOf(origin) === -1){
+// If a specific origin isn´t found on the list of allowed origins
+      let message = 'The CORS policy for this application doesn´t allow access from origin ' + origin;
+      return callback(new Error(message), false);
+    }
+    return callback(null, true);
+  }
+}));
 
 // GET requests
 
@@ -107,30 +118,53 @@ app.get('/genres/:Name', passport.authenticate('jwt', {session: false}), (req, r
   Birthday: Date
 }*/
 
-app.post('/users', passport.authenticate('jwt', {session: false}), (req, res) => {
-  users.findOne({ Username: req.body.Username })
-    .then((user) => {
-      if (user) {
-        return res.status(400).send(req.body.Username + ' already exists');
-      } else {
-        users
-          .create({
-            Username: req.body.Username,
-            Password: req.body.Password,
-            Email: req.body.Email,
-            Birthday: req.body.Birthday
+app.post('/users',
+
+  // Validation logic here for request
+  // you can either use a chain of methods like .not().isEmpty()
+  // which means "opposite of isEmpty" in plain english "is not empty"
+  // or use .isLength({min:5}) which means
+  //minimum value of 5 characters are only allowed
+
+  [
+    check('Username', 'Username is required').isLength({min:5}),
+    check('Username', 'Username contains non alphanumeric characters - not allowed').isAlphanumeric(),
+    check('Password', 'Password is required').not().isEmpty(),
+    check('Email', 'Email does not appear to be valid').isEmail()
+  ], (req, res) => {
+
+    // check the validation object for errors
+    let errors = validationResult(req);
+    
+    if (!errors.isEmpty()) {
+      return res.status(422).json({ errors: errors.array() });
+    }
+
+    let hashedPassword = Users.hashPassword(req.body.Password);
+    users.findOne({ Username: req.body.Username }) // Search to see if a user with the requested username already exists
+      .then((user) => {
+        if (user) {
+        // If the user is found, send a response that it already exists  
+          return res.status(400).send(req.body.Username + ' already exists');
+        } else {
+          users
+            .create({
+              Username: req.body.Username,
+              Password: hashedPassword,
+              Email: req.body.Email,
+              Birthday: req.body.Birthday
+            })
+            .then((user) =>{res.status(201).json(user) })
+          .catch((error) => {
+            console.error(error);
+            res.status(500).send('Error: ' + error);
           })
-          .then((user) =>{res.status(201).json(user) })
-        .catch((error) => {
-          console.error(error);
-          res.status(500).send('Error: ' + error);
-        })
-      }
-    })
-    .catch((error) => {
-      console.error(error);
-      res.status(500).send('Error: ' + error);
-    });
+        }
+      })
+      .catch((error) => {
+        console.error(error);
+        res.status(500).send('Error: ' + error);
+      });
 });
 
 
@@ -256,6 +290,7 @@ app.use((err, req, res, next) => {
 
 // listen for requests
 
-app.listen(8080, () => {
-  console.log('Your app is listening on port 8080.');
+const port = process.env.PORT || 8080;
+app.listen( port, '0.0.0.0', () => {
+  console.log('Listening on Port ' + port);
 });
